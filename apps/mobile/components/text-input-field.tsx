72a01e6178b0028add48a548108dc71326cc20ee
Pranav Pakanati <pranav.pakanati@gmail.com>
2026-02-23 00:13:39 -0600

    fix(mobile): apply line height text input fix only on ios

--- apps/mobile/components/text-input-field.tsx
+++ apps/mobile/components/text-input-field.tsx
@@ -1,9 +1,14 @@
-import { Text, TextInput, View } from "react-native";
+import { Platform, Text, TextInput, View } from "react-native";
 
 const TextInputField = ({
   fieldName,
   ...props
 }: { fieldName: string } & React.ComponentProps<typeof TextInput>) => {
+  let _props = { ...props };
+  if (Platform.OS === "ios") {
+    _props = { ..._props, style: { lineHeight: 0 } };
+  }
+
   return (
     <View className="flex-col gap-2">
       <Text className="text-lg font-semibold text-gray-900">{fieldName}</Text>
@@ -11,9 +16,6 @@ const TextInputField = ({
         className="bg-gray-50 border border-gray-200 text-gray-900 text-lg rounded-lg t-colors focus:ring-ut-blue focus:border-ut-blue block w-full p-4"
         {...props}
         placeholderTextColor={"#6b7280"}
-        style={{
-          lineHeight: 0,
-        }}
       />
     </View>
   );
